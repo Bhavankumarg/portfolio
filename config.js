3aa6e136ced1ea225fe26e4fa4541b0ff33c18d0
Bhavankumar Ganesan <bhavankumarganesan@Bhavankumars-MacBook-Air.local>
2026-05-16 17:53:10 +0530

    g removed

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import { HiCode, HiCube, HiDatabase, HiMail, HiBriefcase } from "react-icons/hi"
 
 export const config = {
     developer: {
-        name: "Bhavan Kumar G",
+        name: "Bhavan Kumar",
         
     },
     social: {
